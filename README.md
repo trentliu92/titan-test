db4fb71e05b1667c1c3e7757f9aa070ec0c34293
Trent Liu <tliu@titanhst.com>
2020-01-23 11:43:35 -0800

    updated functional component to class

--- README.md
+++ README.md
@@ -3,20 +3,20 @@
 This is a 1-hour coding challenge designed to test the basic principles of ReactJS and CSS. You do not have to follow the project structure given to you. Feel free to organize your project however you'd like.
 
 ## Criteria
-Try and tackle as many bullet points as you can before your time is up
+Try and tackle as many bullet points as you can before your time is up.
 
-1. Your card must resemble the design presented in Adobe XD
+1. Your card must resemble the design presented in Adobe XD. It does not have to be pixel perfect
 2. Your feed must pull and append 5 new results every 5 seconds
 3. Your feed must be able to filter by three choices from a select menu:
-	a.  No Gender
+	a.  No Filter
 	b.  Male
 	c.  Female
 4. [optional] The card, on click, will permanently delete from the feed
 
 ## Other Notes
+The Styled Components library has been installed. You do not have to use this if you're more comfortable with something else.
+
 The link to the design document:
 https://xd.adobe.com/view/d4f64862-803b-4d42-7cee-c7aebd2edcf5-a2c7/
 
 You are allowed to use any online resource.
-
-We used a **React Hook** as a quick and easy way to illustrate an API call. If you feel more comfortable using component classes, feel free to do so.
--- src/App.js
+++ src/App.js
@@ -1,56 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 // api
 import { PeopleApi } from './services/PeopleApi.js';
 
 // style
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
-  const [ people, setPeople ] = useState(null);
+const SelectContainer = styled.select`
+  width: 160px;
+  height: 30px;
+`
 
-  // fetches data on component mount
-  useEffect(() => {
-    async function fetchPeople() {
-      try {
-        const res = await PeopleApi.getPeople();
-        setPeople(res);
-      } catch (error) {
-        console.error(error);
-      }
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      people: [],
     }
+  }
 
-    fetchPeople();
-  }, []);
+  componentDidMount = () => {
+    this.fetchPeople();
+  }
 
-  const renderPeople = () => {
-    if(people) {
-      return people.map((person) => {
-        const name = `${person.name.first} ${person.name.last}`
+  fetchPeople = async () => {
+    try {
+      const res = await PeopleApi.getPeople();
+      this.setState({
+        people: res
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-        return (
-          <div key={person.login.uuid}>
-            <div style={{ width: '100px' }}>
-              <img style={{ width: '100%' }} src={person.picture.medium} alt="" />
-            </div>
+  handleChange = (val) => {
 
+  }
+
+  renderPeople = () => {
+    if(this.state.people) {
+      return this.state.people.map((person) => (
+          <div>
+            <img src={person.picture.medium} alt=""/>
             <div>
-              { name }
+              { `${person.name.first} ${person.name.last}` }
             </div>
           </div>
-        );
-      });
+      ));
     }
 
     return null;
   }
 
-  return (
-    <div>
-      { renderPeople() }
-    </div>
-  );
-}
+  render = () => {
+    return (
+      <div>
+        <SelectContainer onChange={(val) => this.handleChange(val)}>
+            <option>No Filter</option>
+            <option>Male</option>
+            <option>Female</option>
+        </SelectContainer>
 
-export default App;
+        { this.renderPeople() }
+      </div>
+    );
+  }
+
+}
